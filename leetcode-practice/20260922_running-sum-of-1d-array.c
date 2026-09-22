3b9ecd5e0964fd415c38192acf9d93acb5fa86a3
Ryan Wu <83266875+5times-2@users.noreply.github.com>
2026-09-22 22:45:30 +0800

    Add notes

--- leetcode-practice/20260922_running-sum-of-1d-array.c
+++ leetcode-practice/20260922_running-sum-of-1d-array.c
@@ -2,11 +2,13 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* runningSum(int* nums, int numsSize, int* returnSize) {
-    int* x = malloc(numsSize * sizeof(int));
+    int* result = malloc(numsSize * sizeof(int));
     *returnSize = numsSize;
-    x[0] = nums[0];
-    for (int c = 1; c < numsSize; c++) {
-        x[c] = x[c - 1] + nums[c];
+    result[0] = nums[0];
+    for (int c = 1; c < numsSize; c++) { 
+    // Note: numsSize is the number of elements, while array indices start from 0.
+    // E.g. if numsSize == 4, the last valid element is result[3], so c must not exceed 3.
+        result[c] = result[c - 1] + nums[c];
     }
-    return x;
+    return result;
 }
